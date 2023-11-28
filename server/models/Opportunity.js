096f67241d5bb5823acff53046f4ddc5fc31858d
Juli Khosla <julikhosla17@gmail.com>
2023-11-28 14:40:52 -0800

    Added collection name

--- server/models/Opportunity.js
+++ server/models/Opportunity.js
@@ -1,14 +1,17 @@
 const mongoose = require("mongoose");
 
-const opportunitySchema = new mongoose.Schema({
-  name: String,
-  clientName: String,
-  stage: String,
-  probability: Number,
-  dealAmount: Number,
-  createdOn: Date,
-});
+const opportunitySchema = new mongoose.Schema(
+  {
+    name: String,
+    clientName: String,
+    stage: String,
+    probability: Number,
+    dealAmount: Number,
+    createdOn: Date,
+  },
+  { collection: "Opportunities" }
+);
 
-const Opportunity = mongoose.model("Opportunity", opportunitySchema);
+const Opportunity = mongoose.model("Opportunities", opportunitySchema);
 
 module.exports = Opportunity;
