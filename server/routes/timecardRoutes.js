6ef4927c7422f7b4298138fcfee012460a359f80
Juli Khosla <julikhosla17@gmail.com>
2024-12-09 15:32:51 -0800

    Remove token from timecards

--- server/routes/timecardRoutes.js
+++ server/routes/timecardRoutes.js
@@ -2,9 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const timecardController = require("../controllers/timecardController");
-const { authenticateToken } = require("../middleware/auth");
 
-router.get("/:companyName", authenticateToken, timecardController.getTimecard);
+router.get("/:companyName", timecardController.getTimecard);
 
 router.post("/", timecardController.createTimecard);
 
