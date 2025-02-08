56b4bb821d30c030b96db5550a6887d686dd319d
Juli Khosla <julikhosla17@gmail.com>
2025-02-07 16:56:24 -0800

    Added library crypto

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const crypto = require("crypto");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
