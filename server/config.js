977ac77d5947a94e190bbe1187c6fe179310be1c
Juli Khosla <julikhosla17@gmail.com>
2025-02-10 20:06:51 -0800

    fix cor

--- server/config.js
+++ server/config.js
@@ -9,7 +9,7 @@ const corsOptions = {
 		"http://localhost:3001",
 		"https://businessn-erp.onrender.com",
 		"https://crm-backend-874t.onrender.com",
-		"https://test-crm-staging.netlify.app/",
+		"https://test-crm-staging.netlify.app",
 	],
 	methods: ["GET", "POST", "PUT", "DELETE"],
 	credentials: true, // Allow cookies to be sent
