6a88469a684e712c8536499b355a6a205ca9667a
Juli Khosla <julikhosla17@gmail.com>
2025-02-10 20:03:50 -0800

    Added lcoalstaging access

--- server/config.js
+++ server/config.js
@@ -9,6 +9,7 @@ const corsOptions = {
 		"http://localhost:3001",
 		"https://businessn-erp.onrender.com",
 		"https://crm-backend-874t.onrender.com",
+		"https://test-crm-staging.netlify.app/",
 	],
 	methods: ["GET", "POST", "PUT", "DELETE"],
 	credentials: true, // Allow cookies to be sent
