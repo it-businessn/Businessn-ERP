35103121156050e10360fec771d3f274cbcfeddf
Juli Khosla <julikhosla17@gmail.com>
2025-03-18 12:35:23 -0700

    redirect login on access token invalid

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -47,6 +47,7 @@ export const setupAxiosInterceptors = (setSessionExpired) => {
 				}
 			} else {
 				console.error("Refresh token invalid or expired", error);
+				if (error.response.data.message.includes("token")) window.location.href = "/login";
 			}
 
 			return Promise.reject(error);
