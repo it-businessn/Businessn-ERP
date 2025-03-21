51cd1e2c5de3ce508b95174e70c2bcfed9cf82d3
Juli Khosla <julikhosla17@gmail.com>
2025-03-20 18:17:26 -0700

    updated company users data

--- client/src/hooks/useCompanyEmployees.js
+++ client/src/hooks/useCompanyEmployees.js
@@ -8,6 +8,11 @@ const useCompanyEmployees = (company, deptName) => {
 		const fetchAllEmployees = async () => {
 			try {
 				const { data } = await UserService.getAllCompanyUsers(company, deptName);
+				data.map((emp) => {
+					emp.fullName = emp?.empId?.fullName;
+					emp._id = emp?.empId?._id;
+					return emp;
+				});
 				setEmployees(data);
 			} catch (error) {
 				console.error(error);
