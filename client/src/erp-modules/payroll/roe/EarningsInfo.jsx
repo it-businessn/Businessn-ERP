7d6252b5fd9057e6d6cc6f0eb892f8471f576479
Juli Khosla <julikhosla17@gmail.com>
2025-03-21 17:32:35 -0700

    fixed earnings info table

--- client/src/erp-modules/payroll/roe/EarningsInfo.jsx
+++ client/src/erp-modules/payroll/roe/EarningsInfo.jsx
@@ -12,11 +12,14 @@ import { convertDecimal, getAmount } from "utils/convertAmt";
 
 const EarningsInfo = ({ company, tabId, handleNext }) => {
 	const [earningsData, setEarningsData] = useState([]);
+	const [set1, setSet1] = useState([]);
+	const [set2, setSet2] = useState([]);
 	const [totalInsurableHours, setTotalInsurableHours] = useState(0);
 	const [totalInsurableEarnings, setTotalInsurableEarnings] = useState(0);
 	const cols = ["ROE PP", "Payroll PP", "PP End Date", "Insurable Earnings", "Insurable Hours"];
 
 	const roeEmpId = LocalStorageService.getItem("roeEmpId");
+
 	useEffect(() => {
 		const fetchEmpPayStubs = async () => {
 			try {
@@ -51,6 +54,26 @@ const EarningsInfo = ({ company, tabId, handleNext }) => {
 		if (roeEmpId) fetchEmpPayStubs();
 	}, [company]);
 
+	useEffect(() => {
+		if (earningsData.length) {
+			const evenRecords = [];
+			const oddRecords = [];
+			let evenIndex = 1;
+			let oddIndex = 2;
+			earningsData.forEach((record, index) => {
+				if (index % 2 === 0) {
+					evenRecords.push({ ...record, newIndex: evenIndex });
+					evenIndex += 2;
+				} else {
+					oddRecords.push({ ...record, newIndex: oddIndex });
+					oddIndex += 2;
+				}
+			});
+			setSet1(evenRecords);
+			setSet2(oddRecords);
+		}
+	}, [earningsData]);
+
 	const handleConfirm = () => {
 		handleNext(tabId);
 	};
@@ -76,11 +99,11 @@ const EarningsInfo = ({ company, tabId, handleNext }) => {
 							</Tr>
 						</Thead>
 						<Tbody>
-							{earningsData?.map(
-								({ payPeriodNum, payPeriodEndDate, currentGrossPay, insurableHours }, index) => (
-									<Tr key={`${payPeriodNum}_${index + 1}`}>
+							{set1?.map(
+								({ payPeriodNum, payPeriodEndDate, currentGrossPay, insurableHours, newIndex }) => (
+									<Tr key={`${payPeriodNum}_${newIndex}`}>
 										<Td py={0}>
-											<NormalTextTitle align="center" title={index + 1} />
+											<NormalTextTitle align="center" title={newIndex} />
 										</Td>
 
 										<Td py={1}>
@@ -121,11 +144,11 @@ const EarningsInfo = ({ company, tabId, handleNext }) => {
 							</Tr>
 						</Thead>
 						<Tbody>
-							{earningsData?.map(
-								({ payPeriodNum, payPeriodEndDate, currentGrossPay, insurableHours }, index) => (
-									<Tr key={`${payPeriodNum}_${index + 1}`}>
+							{set2?.map(
+								({ payPeriodNum, payPeriodEndDate, currentGrossPay, insurableHours, newIndex }) => (
+									<Tr key={`${payPeriodNum}_${newIndex}`}>
 										<Td py={0}>
-											<NormalTextTitle align="center" title={index + 1} />
+											<NormalTextTitle align="center" title={newIndex} />
 										</Td>
 
 										<Td py={1}>
