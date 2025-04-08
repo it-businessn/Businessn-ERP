393cb89f47326eeefcdcdd5be9c3baa84bb45d48
Juli Khosla <juli@spotonautomation.com>
2025-04-07 23:30:29 -0700

    fixed journal report

--- client/src/erp-modules/payroll/process-payroll/preview-reports/JournalsReportModal.jsx
+++ client/src/erp-modules/payroll/process-payroll/preview-reports/JournalsReportModal.jsx
@@ -1,4 +1,4 @@
-import { SimpleGrid, Stack } from "@chakra-ui/react";
+import { Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
 import Loader from "components/Loader";
 import ModalLayout from "components/ui/modal/ModalLayout";
 import TextTitle from "components/ui/text/TextTitle";
@@ -11,6 +11,39 @@ const JournalsReportModal = ({
 	size = "6xl",
 	title = "Journal Entry Report",
 }) => {
+	const rowData = [
+		// ["", "", "Employee Disbursements", "-6107.89", ""],
+		// ["", "", "Government Remittances", "-3035.56", ""],
+		["", "1001", "Bank", "Funding Withdrawal", "", reportData?.netFundingWithdrawals.toFixed(2)],
+		["", "", "", "Net Payroll Payable", "", ""],
+		["", "2105", "EI Payable", "EI Payable", reportData?.totalEIPayable?.toFixed(2), ""],
+		["", "2106", "CPP Payable", "CPP Payable", reportData?.totalCPPPayable?.toFixed(2), ""],
+		[
+			"",
+			"2110",
+			"Income Tax Payable",
+			"Income Tax Payable",
+			reportData?.totalIncomeTaxPayable?.toFixed(2),
+			"",
+		],
+		["", "5321", "Payroll Expense", "Service Charges", "0", ""],
+	];
+	let counter = 10;
+	reportData.totalCredit = reportData?.netFundingWithdrawals;
+	reportData.totalDebit =
+		reportData?.totalEIPayable + reportData?.totalCPPPayable + reportData.totalIncomeTaxPayable;
+	reportData?.departmentBreakDown?.map((dept, index) => {
+		dept.code = `${index + 1 * counter}${counter++}`;
+		reportData.totalCredit +=
+			dept?.CPPPayable +
+			dept.EIPayable +
+			dept.employeeCPPContribution +
+			dept.employeeEIContribution +
+			dept.incomeTaxContribution;
+		reportData.totalDebit +=
+			dept?.grossWageExpense + dept.employerCPPBenefitExpense + dept.employerEIBenefitExpense;
+		return dept;
+	});
 	return (
 		<ModalLayout
 			title={<TextTitle title={title} />}
@@ -23,82 +56,116 @@ const JournalsReportModal = ({
 		>
 			{!reportData && <Loader />}
 			{reportData && (
-				<Stack>
-					<SimpleGrid mb={"1em"} columns={{ base: 6 }} spacing="1em">
-						<TextTitle title="" />
-						<TextTitle title="Client Account Code" />
-						<TextTitle title="Code Name" />
-						<TextTitle title="Description" />
-						<TextTitle title="Debit" />
-						<TextTitle title="Credit" />
-						{/* 
-						<TextTitle title="" />
-						<TextTitle title="Client Account Code" />
-						<TextTitle title="Code Name" />
-						<TextTitle title="Description" />
-						<TextTitle title="Debit" />
-						<TextTitle title="Credit" /> */}
-
-						<TextTitle title="" />
-						<TextTitle title="1000" />
-						<TextTitle title="Bank" />
-						<TextTitle title="Funding Withdrawal " />
-						<TextTitle title="" />
-						<TextTitle title={reportData?.netFundingWithdrawals?.toFixed(2)} />
-
-						<TextTitle title="" />
-						<TextTitle title="" />
-						<TextTitle title="" />
-						<TextTitle title="Net Payroll Payable" />
-						<TextTitle title="" />
-						<TextTitle title="" />
-
-						<TextTitle title="" />
-						<TextTitle title="2105" />
-						<TextTitle title="EI Payable" />
-						<TextTitle title="EI Payable" />
-						<TextTitle title={reportData?.totalEIPayable} />
-						<TextTitle title="" />
-
-						<TextTitle title="" />
-						<TextTitle title="2106" />
-						<TextTitle title="CPP Payable" />
-						<TextTitle title="CPP Payable" />
-						<TextTitle title={reportData?.totalCPPPayable} />
-						<TextTitle title="" />
-						<TextTitle title="" />
-						<TextTitle title="2110" />
-						<TextTitle title="Income Tax Payable" />
-						<TextTitle title="Income Tax Payable " />
-						<TextTitle title={reportData?.totalIncomeTaxPayable} />
-						<TextTitle title="" />
-
-						<TextTitle title="" />
-						<TextTitle title="5321" />
-						<TextTitle title="Payroll Expense" />
-						<TextTitle title="Service Charges" />
-						<TextTitle title="0" />
-						<TextTitle title="" />
-						{reportData?.departmentBreakDown?.map((dept) => (
+				<Table variant="simple" size="small">
+					<Thead>
+						<Tr>
+							<Th></Th>
+							<Th>Client Account Code </Th>
+							<Th>Code Name</Th>
+							<Th>Description</Th>
+							<Th isNumeric>Debit</Th>
+							<Th isNumeric>Credit</Th>
+						</Tr>
+					</Thead>
+					<Tbody>
+						{rowData.map((row, idx) => (
+							<Tr key={idx}>
+								{row.map((cell, cellIdx) => (
+									<Td key={cellIdx} isNumeric={cellIdx > 3}>
+										{cell}
+									</Td>
+								))}
+							</Tr>
+						))}
+						{reportData?.departmentBreakDown?.map((dept, index) => (
 							<React.Fragment key={dept.department}>
-								<TextTitle title="" />
-								<TextTitle title="" />
-								<TextTitle title="" />
-								<TextTitle title="" />
-								<TextTitle title="" />
-								<TextTitle title="" />
-								<TextTitle title={dept.department} />
-								<TextTitle title="5314" />
-								<TextTitle title={`${dept.department} Wages`} />
-								<TextTitle title="Gross Wage Expense" />
-								<TextTitle title={dept?.grossWageExpense} />
-								<TextTitle title="" />
-								{/* CPPPayable,EIPayable,department,employeeCPPContribution,employeeEIContribution,employerCPPBenefitExpense,employerEIBenefitExpense,grossWageExpense,incomeTaxContribution */}
+								<Tr>
+									<Td>Department:</Td>
+									<Td />
+									<Td />
+									<Td />
+									<Td />
+									<Td />
+								</Tr>
+								<Tr>
+									<Td>{dept.department}</Td>
+									<Td>{`${dept.code}${index + 1}`}</Td>
+									<Td>{`${dept.department} Wages`}</Td>
+									<Td>Gross Wage Expense</Td>
+									<Td isNumeric>{dept.grossWageExpense?.toFixed(2)}</Td>
+									<Td isNumeric></Td>
+								</Tr>
+								<Tr>
+									<Td></Td>
+									<Td>{`${dept.code}${index + 2}`}</Td>
+									<Td>{`${dept.department} Benefits`}</Td>
+									<Td> CPP Benefits Expense</Td>
+									<Td isNumeric>{dept.employerCPPBenefitExpense?.toFixed(2)}</Td>
+									<Td isNumeric></Td>
+								</Tr>
+								<Tr>
+									<Td></Td>
+									<Td>{`${dept.code}${index + 3}`}</Td>
+									<Td>{`${dept.department} Benefits`}</Td>
+									<Td> EI Benefits Expense</Td>
+									<Td isNumeric>{dept.employerEIBenefitExpense?.toFixed(2)}</Td>
+									<Td isNumeric></Td>
+								</Tr>
+								<Tr>
+									<Td></Td>
+									<Td>{`${dept.code}${index + 4}`}</Td>
+									<Td>CPP Payable</Td>
+									<Td>CPP Payable</Td>
+									<Td isNumeric></Td>
+									<Td isNumeric>{dept.CPPPayable?.toFixed(2)}</Td>
+								</Tr>
+								<Tr>
+									<Td></Td>
+									<Td>{`${dept.code}${index + 5}`}</Td>
+									<Td>EI Payable</Td>
+									<Td>EI Payable</Td>
+									<Td isNumeric></Td>
+									<Td isNumeric>{dept.EIPayable?.toFixed(2)}</Td>
+								</Tr>
+								<Tr>
+									<Td />
+									<Td /> <Td /> <Td /> <Td /> <Td />
+								</Tr>
+								<Tr>
+									<Td></Td>
+									<Td>{`${dept.code}${index + 6}`}</Td>
+									<Td>EI Payable</Td>
+									<Td> EI Employee Contribution</Td>
+									<Td isNumeric></Td>
+									<Td isNumeric>{dept.employeeEIContribution?.toFixed(2)}</Td>
+								</Tr>
+								<Tr>
+									<Td></Td>
+									<Td>{`${dept.code}${index + 7}`}</Td>
+									<Td>CPP Payable</Td>
+									<Td> CPP Employee Contribution</Td>
+									<Td isNumeric></Td>
+									<Td isNumeric>{dept.employeeCPPContribution?.toFixed(2)}</Td>
+								</Tr>
+								<Tr>
+									<Td></Td>
+									<Td>{`${dept.code}${index + 7}`}</Td>
+									<Td>Income Tax Payable</Td>
+									<Td> Income Tax Contribution</Td>
+									<Td isNumeric></Td>
+									<Td isNumeric>{dept.incomeTaxContribution?.toFixed(2)}</Td>
+								</Tr>
 							</React.Fragment>
 						))}
-						{/* totalCredit,totalDebit,totalFundingWithDrawals,totalServiceCharges */}
-					</SimpleGrid>
-				</Stack>
+
+						<Tr mt={3}>
+							<Td>IN BALANCE</Td>
+							<Td /> <Td /> <Td />
+							<Td isNumeric>{reportData.totalDebit?.toFixed(2)}</Td>
+							<Td isNumeric>{reportData.totalCredit?.toFixed(2)}</Td>
+						</Tr>
+					</Tbody>
+				</Table>
 			)}
 		</ModalLayout>
 	);
