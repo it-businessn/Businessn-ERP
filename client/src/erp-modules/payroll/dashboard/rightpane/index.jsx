4fa1e7a48471c77aa543c722a1911e9084841121
Juli Khosla <juli@spotonautomation.com>
2025-05-15 13:15:40 -0700

    Added placeholder items dashboard payroll

--- client/src/erp-modules/payroll/dashboard/rightpane/index.jsx
+++ client/src/erp-modules/payroll/dashboard/rightpane/index.jsx
@@ -1,7 +1,9 @@
 import BoxCard from "components/ui/card";
-import ChatMessages from "erp-modules/sales/dashboard/rightpane/ChatMessages";
 import MiniCalendar from "erp-modules/sales/dashboard/rightpane/MiniCalendar";
+import { useEffect, useState } from "react";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import TaskService from "services/TaskService";
+import AlertActivity from "./AlertActivity";
 import PayrollUserStatInfo from "./PayrollUserStatInfo";
 
 const RightPane = ({
@@ -12,6 +14,66 @@ const RightPane = ({
 	closestRecord,
 	closestRecordIndex,
 }) => {
+	const [tickets, setTickets] = useState(null);
+	const [chat, setChat] = useState(null);
+	const [tasks, setTasks] = useState(null);
+	const [appointments, setAppointments] = useState(null);
+
+	useEffect(() => {
+		const fetchAllUserTasks = async () => {
+			try {
+				const { data } = await TaskService.getTaskByAssignee({
+					name: selectedUser?.fullName,
+					company,
+				});
+				setTasks(data);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+		const fetchAllUserChat = async () => {
+			try {
+				// const { data } = await CommunicationService.getUserConversations({
+				// 	name: selectedUser?.fullName,
+				// 	company,
+				// });
+				const { data } = await TaskService.getTaskByAssignee({
+					name: selectedUser?.fullName,
+					company,
+				});
+				setChat(data);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+		const fetchAllUserAppointment = async () => {
+			try {
+				const { data } = await TaskService.getTaskByAssignee({
+					name: selectedUser?.fullName,
+					company,
+				});
+				setAppointments(data);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+		const fetchAllUserTickets = async () => {
+			try {
+				const { data } = await TaskService.getTaskByAssignee({
+					name: selectedUser?.fullName,
+					company,
+				});
+				setTickets(data);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+		fetchAllUserTickets();
+		fetchAllUserAppointment();
+		fetchAllUserChat();
+		fetchAllUserTasks();
+	}, [selectedUser]);
+
 	return (
 		<BoxCard>
 			<PayrollUserStatInfo
@@ -22,7 +84,8 @@ const RightPane = ({
 				closestRecordIndex={closestRecordIndex}
 			/>
 			<MiniCalendar user={selectedUser} company={company} isPayrollDashboard />
-			<ChatMessages userId={selectedPayGroup?._id} company={company} />
+			{/* <ChatMessages userId={selectedPayGroup?._id} company={company} /> */}
+			<AlertActivity tickets={tickets} chat={chat} tasks={tasks} appointments={appointments} />
 		</BoxCard>
 	);
 };
