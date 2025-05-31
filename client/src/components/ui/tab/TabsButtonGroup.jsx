c6e987d142761d4ecf2e2aba1250ee674805275c
Juli Khosla <juli@spotonautomation.com>
2025-05-30 21:40:32 -0700

    Updated tickets badge count

--- client/src/components/ui/tab/TabsButtonGroup.jsx
+++ client/src/components/ui/tab/TabsButtonGroup.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, ButtonGroup, Icon } from "@chakra-ui/react";
+import { Badge, Box, Button, ButtonGroup, Icon } from "@chakra-ui/react";
 
 const TabsButtonGroup = ({ isOutlineTab, tabs, setViewMode, viewMode, mt, w, mb = 4 }) => {
 	const getColor = (type) =>
@@ -19,22 +19,51 @@ const TabsButtonGroup = ({ isOutlineTab, tabs, setViewMode, viewMode, mt, w, mb 
 	return (
 		<Box mb={mb} mt={mt} bg={"var(--main_color)"} borderRadius={"1em"} px="5px" w={w}>
 			<ButtonGroup variant="solid" p={0} m={0}>
-				{tabs?.map(({ type, icon, highlightColor }) => (
-					<Button
-						key={type}
-						size={"sm"}
-						onClick={() => setViewMode(type)}
-						color={getColor(type)}
-						bg={getBgColor(type, highlightColor)}
-						borderBottom={border(type)}
-						borderRadius={!isOutlineTab && "1em"}
-						variant={isOutlineTab ? "ghost" : "solid"}
-						fontWeight={isOutlineTab || viewMode === type ? "bold" : "normal"}
-						_hover={{ bg: getBgColor(type, highlightColor), color: "var(--main_color_black)" }}
-					>
-						{icon ? <Icon as={icon} boxSize="5" /> : type}
-					</Button>
-				))}
+				{tabs?.map(({ type, count, icon, highlightColor }) =>
+					type === "Tickets" ? (
+						<Box display="flex" alignItems="center" gap="0">
+							<Button
+								key={type}
+								size={"sm"}
+								onClick={() => setViewMode(type)}
+								color={getColor(type)}
+								bg={getBgColor(type, highlightColor)}
+								borderBottom={border(type)}
+								borderRadius={!isOutlineTab && "1em"}
+								variant={isOutlineTab ? "ghost" : "solid"}
+								fontWeight={isOutlineTab || viewMode === type ? "bold" : "normal"}
+								_hover={{ bg: getBgColor(type, highlightColor), color: "var(--main_color_black)" }}
+							>
+								{icon ? <Icon as={icon} boxSize="5" /> : type}
+							</Button>
+							<Badge
+								pos="relative"
+								mt={"-20px"}
+								ml={"-8px"}
+								colorScheme="red"
+								borderRadius="full"
+								px="2"
+							>
+								{count}
+							</Badge>
+						</Box>
+					) : (
+						<Button
+							key={type}
+							size={"sm"}
+							onClick={() => setViewMode(type)}
+							color={getColor(type)}
+							bg={getBgColor(type, highlightColor)}
+							borderBottom={border(type)}
+							borderRadius={!isOutlineTab && "1em"}
+							variant={isOutlineTab ? "ghost" : "solid"}
+							fontWeight={isOutlineTab || viewMode === type ? "bold" : "normal"}
+							_hover={{ bg: getBgColor(type, highlightColor), color: "var(--main_color_black)" }}
+						>
+							{icon ? <Icon as={icon} boxSize="5" /> : type}
+						</Button>
+					),
+				)}
 			</ButtonGroup>
 		</Box>
 	);
